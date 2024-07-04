53f779f8a58bbb1856528d125a051a7b4a9b1c9f
Mohamed Rifkan <59793466+rifkan-py@users.noreply.github.com>
2024-07-04 13:11:05 +0530

    updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"time"
-
-	"github.com/rifkan-py/portion-share/internal/server"
-)
+import "fmt"
 
 func main() {
-	r := server.SetupRoutes()
-
-	srv := &http.Server{
-		ReadTimeout:       15 * time.Second,
-		WriteTimeout:      15 * time.Second,
-		ReadHeaderTimeout: 15 * time.Second,
-		IdleTimeout:       2 * time.Hour,
-		MaxHeaderBytes:    10 << 20,
-		Handler:           r,
-		Addr:              ":8080",
-	}
-
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("running ...")
 }
