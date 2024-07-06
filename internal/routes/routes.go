1facdf597933ed5965344dc3793159e06eb5be34
Mohamed Rifkan <59793466+rifkan-py@users.noreply.github.com>
2024-07-06 13:24:01 +0530

    updated

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,7 +2,11 @@ package routes
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
+	"path/filepath"
+	"runtime"
+	"text/template"
 
 	"github.com/rifkan-py/portion-share/internal/handlers"
 )
@@ -11,6 +15,23 @@ type ServerConfig struct {
 	DB *sql.DB
 }
 
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basePath   = filepath.Dir(b)
+)
+
 func SetupRoutes(r *http.ServeMux) {
+	r.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		templatePath := filepath.Join(basePath, "../templates/index.html")
+		tmpl, err := template.ParseFiles(templatePath)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(templatePath)
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		tmpl.Execute(w, nil)
+	})
 	r.HandleFunc("GET /upload", handlers.UploadHandler)
 }
